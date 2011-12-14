eed1679ef5da1752f91f335ef2ab5799ef254569
Hallison Batista <hallison.batista@gmail.com>
2011-12-14 09:56:29 -0400

    config for RVM was simplified

--- dot/profile.d/rvm.sh
+++ dot/profile.d/rvm.sh
@@ -1,10 +1,3 @@
 # RVM Settings
-if test -s /etc/rvmrc; then
-  . /etc/rvmrc
-  . "$rvm_path/scripts/rvm"
-elif test -s "$HOME/.rvm/scripts/rvm"; then
-  . "$HOME/.rvm/scripts/rvm"
-else
-  : rvm not found
-fi
+test -s "$HOME/.rvm/scripts/rvm" && . "$HOME/.rvm/scripts/rvm"
 
