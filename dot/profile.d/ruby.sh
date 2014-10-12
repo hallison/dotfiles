3314b58cb217f93360d0fac8daf77a59df2718f6
Hallison Batista <hallisonbatista@gmail.com>
2014-10-12 12:15:54 -0400

    RBEnv configuration

--- dot/profile.d/ruby.sh
+++ dot/profile.d/ruby.sh
@@ -2,6 +2,11 @@ export RUBY_ENGINE=${RUBY_ENGINE:-ruby}
 export RUBY_VERSION=${RUBY_VERSION:-2.1.2}
 export GEM_HOME=$HOME/.gem/$RUBY_ENGINE/$RUBY_VERSION
 export GEM_BINDIR=$GEM_HOME/bin
-export PATH=$PATH:$GEM_BINDIR
+export RBENV_HOME=${RBENV_HOME:-$HOME/.rbenv}
+export RBENV_BINDIR=$RBENV_HOME/bin
+export RBENV_PROFILE=$HOME/.rbenvrc
+export PATH=$RBENV_BINDIR:$GEM_BINDIR:$PATH
 
 alias irb='irb -rubygems -rirb/completion'
+
+rbenv init - > $RBENV_PROFILE && source $RBENV_PROFILE
