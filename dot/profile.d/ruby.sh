c3989f1888e16b7033b65472bb6a556673044e80
Hallison Batista <hallisonbatista@gmail.com>
2015-03-18 13:53:42 -0400

    Improvements in Ruby environment

--- dot/profile.d/ruby.sh
+++ dot/profile.d/ruby.sh
@@ -1,12 +1,16 @@
-export RUBY_ENGINE=${RUBY_ENGINE:-ruby}
-export RUBY_VERSION=${RUBY_VERSION:-2.1.2}
-export GEM_HOME=$HOME/.gem/$RUBY_ENGINE/$RUBY_VERSION
-export GEM_BINDIR=$GEM_HOME/bin
 export RBENV_HOME=${RBENV_HOME:-$HOME/.rbenv}
 export RBENV_BINDIR=$RBENV_HOME/bin
 export RBENV_PROFILE=$HOME/.rbenvrc
-export PATH=$RBENV_BINDIR:$GEM_BINDIR:$PATH
-
-alias irb='irb -rubygems -rirb/completion'
+export PATH=$RBENV_BINDIR:$PATH
 
 rbenv init - > $RBENV_PROFILE && source $RBENV_PROFILE
+
+export RUBY_ENGINE=$(ruby -e 'puts RUBY_ENGINE')
+export RUBY_VERSION=$(ruby -e 'puts RUBY_VERSION')
+
+export GEM_HOME=$HOME/.gem/$RUBY_ENGINE/$RUBY_VERSION
+export GEM_BINDIR=$GEM_HOME/bin
+
+export PATH=$GEM_BINDIR:$PATH
+
+alias irb='irb -rubygems -rirb/completion'
