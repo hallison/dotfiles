afb74796dc6e7e093d15735790c87144ebdabad9
Hallison Batista <hallisonbatista@gmail.com>
2021-05-17 01:17:55 -0400

    GoEnv settings

--- go/profile.d/go.sh
+++ go/profile.d/go.sh
@@ -1,6 +1,11 @@
+export GOPATH=$HOME/.go
+export GOBIN=$GOPATH/bin
+
+export GOENV_DISABLE_GOPATH=1
+
 export GOENV_HOME=${GOENV_HOME:-$HOME/.goenv}
-export GOENV_BINDIR=${GOENV_HOME}/bin
-export GOENV_PROFILE=${HOME}/.goenvrc
-export PATH=${GOENV_BINDIR}:${GOROOT}/bin:${GOPATH}/bin:${PATH}
+export GOENV_BINDIR=$GOENV_HOME/bin
+export GOENV_PROFILE=$HOME/.goenvrc
+export PATH=$GOENV_BINDIR:$PATH
 
 goenv init - > $GOENV_PROFILE && source $GOENV_PROFILE
