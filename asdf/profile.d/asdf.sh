f991ee03737545c0b7f28799fea2d4ab35e8e265
Hallison Batista <hallisonbatista@gmail.com>
2024-03-04 20:58:51 -0400

    Fix ASDF VM path

--- asdf/profile.d/asdf.sh
+++ asdf/profile.d/asdf.sh
@@ -3,7 +3,8 @@ export ASDF_BINDIR=$ASDF_HOME/bin
 export ASDF_CONFIG=$ASDF_HOME/asdf.sh
 export ASDF_COMPLETIONS=$ASDF_HOME/completions/asdf.bash
 export ASDF_PROFILE=$HOME/.asdfrc
-export PATH=$ASDF_BINDIR:$PATH
 
 source $ASDF_CONFIG
 source $ASDF_COMPLETIONS
+
+# export PATH=$ASDF_BINDIR:$PATH
